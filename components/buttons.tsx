b68c99373e8319f4e28c349a6028296e57d550b2
Maxime ELIAZORD <mzozord@gmail.com>
2025-04-29 01:01:03 +0200

    Fixed book button z-index

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -5,7 +5,7 @@ function BookButton() {
   return (
     <Link
       href="/calendar"
-      className="fixed bottom-4 left-1/2 -translate-x-1/2 bg-amber-500 hover:bg-amber-600 text-white font-bold text-lg lg:text-xl px-8 py-3 rounded-full shadow-lg transform hover:scale-105 transition-all duration-300 flex items-center gap-2 z-[99]"
+      className="fixed bottom-4 left-1/2 -translate-x-1/2 bg-amber-500 hover:bg-amber-600 text-white font-bold text-lg lg:text-xl px-8 py-3 rounded-full shadow-lg transform hover:scale-105 transition-all duration-300 flex items-center gap-2 z-[2]"
     >
       <FaCalendarCheck />
       <span className="drop-shadow text-nowrap">Réserver maintenant</span>
