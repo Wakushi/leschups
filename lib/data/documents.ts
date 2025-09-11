de1641bc3932800ed54bccce55e4f6c7270fc18d
Maxime ELIAZORD <mzozord@gmail.com>
2025-09-11 20:39:28 +0200

    Updated signin documents

--- lib/data/documents.ts
+++ lib/data/documents.ts
@@ -4,22 +4,22 @@ export type DownloadFile = {
 }
 
 const signInFiles: DownloadFile[] = [
-  // {
-  //   name: "Fiche d'inscription",
-  //   url: "https://firebasestorage.googleapis.com/v0/b/chup-s.appspot.com/o/Fiche%20d'inscription%202024-2025.doc?alt=media&token=93d6706f-64b8-4e5a-a2bd-9ebd30bc906f",
-  // },
-  // {
-  //   name: "Formulaire de droit à l'image",
-  //   url: "https://firebasestorage.googleapis.com/v0/b/chup-s.appspot.com/o/Formulaire%20droit%20a%20l'image%202024-2025.doc?alt=media&token=1a2d7abf-d43b-4847-a320-c177256f6313",
-  // },
-  // {
-  //   name: "Modalités de règlement",
-  //   url: "https://firebasestorage.googleapis.com/v0/b/chup-s.appspot.com/o/Modalite%CC%81s%20de%20Re%CC%80glement%202024-2025.docx?alt=media&token=0953a5a8-51bb-472c-9832-1f7c022203c8",
-  // },
-  // {
-  //   name: "Conditions d'inscription",
-  //   url: "https://firebasestorage.googleapis.com/v0/b/chup-s.appspot.com/o/Conditions%20inscription%20%202024-2025.doc?alt=media&token=338ccead-f9da-49c7-b22f-1ec343786307",
-  // },
+  {
+    name: "Fiche d'inscription",
+    url: "https://firebasestorage.googleapis.com/v0/b/chup-s.appspot.com/o/Inscription%2FFiche%20d'inscription%202025-2026.doc?alt=media&token=d4659621-fb9d-4fcb-91f4-9ca12e6be47f",
+  },
+  {
+    name: "Formulaire de droit à l'image",
+    url: "https://firebasestorage.googleapis.com/v0/b/chup-s.appspot.com/o/Inscription%2FFormulaire%20droit%20a%20l'image%202025-2026.doc?alt=media&token=092ba9ba-1bc5-4be8-9a4b-583f602af2a4",
+  },
+  {
+    name: "Modalités de règlement",
+    url: "https://firebasestorage.googleapis.com/v0/b/chup-s.appspot.com/o/Inscription%2FModalite%CC%81s%20de%20Re%CC%80glement%202025-2026.docx?alt=media&token=d11e0413-a96e-4616-bd70-f66e4235752c",
+  },
+  {
+    name: "Conditions d'inscription",
+    url: "https://firebasestorage.googleapis.com/v0/b/chup-s.appspot.com/o/Inscription%2FConditions%20inscription%20%202025-2026.doc?alt=media&token=9cd377fd-31e1-4325-83cd-904c5eb9e019",
+  },
   {
     name: "RIB de l'association",
     url: "https://firebasestorage.googleapis.com/v0/b/chup-s.appspot.com/o/21%20RIB%20CREDIT%20MUTUEL.pdf?alt=media&token=ccc2548e-2785-4f88-9dd1-7106735dc41b",
@@ -27,10 +27,10 @@ const signInFiles: DownloadFile[] = [
 ]
 
 const dateFiles: DownloadFile[] = [
-  // {
-  //   name: "Planning des répétitions / cours",
-  //   url: "https://firebasestorage.googleapis.com/v0/b/chup-s.appspot.com/o/2024-2025%20Planning%20cours.pdf?alt=media&token=169ef0b7-76f9-437d-8b6e-e5a79419a14b",
-  // },
+  {
+    name: "Planning des répétitions / cours",
+    url: "https://firebasestorage.googleapis.com/v0/b/chup-s.appspot.com/o/Inscription%2F2025-2026%20Calendrier%20des%20salles.pdf?alt=media&token=4d4d94a8-4d4b-4e8e-a664-8d5e96f1e873",
+  },
 ]
 
 const theaterFiles: DownloadFile[] = [
